4b878a38cb63a36dff30357258683422f4589221
anitarua <anita@momentohq.com>
2023-11-06 14:52:25 -0800

    feat: protos

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "client-sdk-swift",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "client-sdk-swift",
+            targets: ["client-sdk-swift"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "client-sdk-swift"),
+        .testTarget(
+            name: "client-sdk-swiftTests",
+            dependencies: ["client-sdk-swift"]),
+    ]
+)
