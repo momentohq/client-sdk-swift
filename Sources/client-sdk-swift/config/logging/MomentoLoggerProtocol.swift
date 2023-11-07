35027469f25e96a9cb75d78f09505f91ecbdb930
anitarua <anita@momentohq.com>
2023-11-07 14:35:55 -0800

    momento logger interface does not need default logger levels

--- Sources/client-sdk-swift/config/logging/MomentoLoggerProtocol.swift
+++ Sources/client-sdk-swift/config/logging/MomentoLoggerProtocol.swift
@@ -1,7 +1,4 @@
 protocol MomentoLoggerProtocol {
-    var loggerName: String { get }
-    var level: DefaultMomentoLoggerLevel { get }
-    
     func error(msg: String)
     func warn(msg: String)
     func info(msg: String)
@@ -10,6 +7,5 @@ protocol MomentoLoggerProtocol {
 }
 
 protocol MomentoLoggerFactoryProtocol {
-    var level: DefaultMomentoLoggerLevel { get }
     func getLogger(loggerName: String) -> MomentoLoggerProtocol
 }
