61f7071bcaaf9baaf5e5d61486cd072f00eca4c2
anitarua <anita@momentohq.com>
2023-11-08 13:27:31 -0800

    wip: topics publish call

--- Sources/client-sdk-swift/internal/PubsubClient.swift
+++ Sources/client-sdk-swift/internal/PubsubClient.swift
@@ -1,16 +1,51 @@
+import GRPC
+@available(macOS 10.15, *)
 class PubsubClient: PubsubClientProtocol {
     var logger: MomentoLoggerProtocol
     var configuration: TopicClientConfiguration
-//    var client: CacheClient_Pubsub_PubsubNIOClient
-//    var credentialProvider
+    var credentialProvider: CredentialProviderProtocol
+    var sharedChannel: GRPCChannel
+    var eventLoopGroup = PlatformSupport.makeEventLoopGroup(loopCount: 1)
+    var client: CacheClient_Pubsub_PubsubAsyncClient
     
-    init(logger: MomentoLoggerProtocol, configuration: TopicClientConfiguration) {
+    init(logger: MomentoLoggerProtocol, configuration: TopicClientConfiguration, credentialProvider: CredentialProviderProtocol) {
         self.logger = logger
         self.configuration = configuration
-//        self.client = CacheClient_Pubsub_PubsubNIOClient(channel: GRPCChannel())
+        self.credentialProvider = credentialProvider
+        
+        do {
+            self.sharedChannel = try GRPCChannelPool.with(
+                target: .host(credentialProvider.cacheEndpoint, port: 443),
+                transportSecurity: .tls(
+                    GRPCTLSConfiguration.makeClientDefault(compatibleWith: eventLoopGroup)
+                ),
+                eventLoopGroup: self.eventLoopGroup
+                ) { configuration in
+                    // Additional configuration, like keepalive.
+                    // Note: Keepalive should in most circumstances not be necessary.
+                    configuration.keepalive = ClientConnectionKeepalive(
+                        interval: .seconds(15),
+                        timeout: .seconds(10)
+                    )
+                }
+        } catch {
+            fatalError("Failed to open GRPC channel")
+        }
+        
+        self.client = CacheClient_Pubsub_PubsubAsyncClient(channel: self.sharedChannel)
     }
     
     func publish() async -> String {
+        var request = CacheClient_Pubsub__PublishRequest()
+        request.cacheName = "foo"
+        request.topic = "bar"
+        request.value.text = "baz"
+        do {
+            var result = try await self.client.publish(request)
+            print(result)
+        } catch {
+            print(error)
+        }
         return "calling client.publish"
     }
     
--- Sources/client-sdk-swift/momento/TopicClient.swift
+++ Sources/client-sdk-swift/momento/TopicClient.swift
@@ -3,13 +3,17 @@ import Foundation
 public class TopicClient: TopicClientProtocol {
     var logger: MomentoLoggerProtocol
     var pubsubClient: PubsubClientProtocol
+    var credentialProvider: CredentialProviderProtocol
     
-    init(configuration: TopicClientConfiguration, logger: MomentoLoggerProtocol = DefaultMomentoLogger(loggerName: "swift-momento-logger", level: DefaultMomentoLoggerLevel.info)) {
+    @available(macOS 10.15, *)
+    init(configuration: TopicClientConfiguration, credentialProvider: CredentialProviderProtocol, logger: MomentoLoggerProtocol = DefaultMomentoLogger(loggerName: "swift-momento-logger", level: DefaultMomentoLoggerLevel.info)) {
         self.logger = logger
-        self.pubsubClient = PubsubClient(logger: logger, configuration: configuration)
+        self.credentialProvider = credentialProvider
+        self.pubsubClient = PubsubClient(logger: logger, configuration: configuration, credentialProvider: credentialProvider)
     }
     
     public func publish() async -> String {
+        let result = await self.pubsubClient.publish()
         return "publishing"
     }
     
