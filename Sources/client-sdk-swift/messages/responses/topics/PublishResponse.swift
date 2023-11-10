6979edc75fce1518cc98a0ba1fbbf727ba4f0649
anitarua <anita@momentohq.com>
2023-11-10 11:44:39 -0800

    refactored response base

--- Sources/client-sdk-swift/messages/responses/topics/PublishResponse.swift
+++ Sources/client-sdk-swift/messages/responses/topics/PublishResponse.swift
@@ -1,27 +1,5 @@
-public class PublishResponse {}
+public protocol PublishResponse {}
 
 public class PublishSuccess: PublishResponse {}
 
-public class PublishError: ErrorResponseBase & PublishResponse {
-    var error: Error
-    
-    init(error: Error) {
-        self.error = error
-    }
-    
-    public func message() -> String {
-        return "inner wrapped error message"
-    }
-    
-    public func innerException() -> String {
-        return "inner error"
-    }
-    
-    public func errorCode() -> String {
-        return "error code"
-    }
-    
-    public func toString() -> String {
-        return "Publish Error: \(self.message())"
-    }
-}
+public class PublishError: ErrorResponseBase & PublishResponse {}
