e6aa7deba8f260cbaf1308955368921b4555389b
anitarua <anita@momentohq.com>
2023-11-30 10:00:13 -0800

    add ability to publish messages in a loop

--- Examples/demo-chat-app/demo-chat-app/ContentView.swift
+++ Examples/demo-chat-app/demo-chat-app/ContentView.swift
@@ -4,6 +4,7 @@ import momento
 struct ContentView: View {
     @State private var momentoClient: Momento
     @State private var message: String = ""
+    @State private var looping: Bool = false
     @ObservedObject var store: MessageStore
     
     public init(momentoClient: Momento) {
@@ -14,23 +15,51 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Publish New Messages")
-                .font(.headline)
+                .font(.title)
                 .foregroundStyle(Color(red: 196/225, green: 241/225, blue: 53/225))
-            TextField("Enter your message:", text: $message)
+            TextField("Enter your message here", text: $message)
                 .border(.secondary)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .onSubmit {
                     Task {
                         await publish()
+                        message = ""
                     }
                 }
+            if looping == false {
+                Button("Print as a loop") {
+                    looping = true
+                    print("Print as a loop")
+                    Task {
+                        while looping {
+                            await publish()
+                            // wait 1.5 seconds before publishing again
+                            try! await Task.sleep(nanoseconds: 1_500_000_000)
+                        }
+                    }
+                }
+                .padding()
+                .background(Color(red: 196/225, green: 241/225, blue: 53/225))
+                .clipShape(Capsule())
+                .foregroundColor(.black)
+            } else {
+                Button("Stop looping") {
+                    looping = false
+                    message = ""
+                    print("Stop looping")
+                }
+                .padding()
+                .background(Color(red: 196/225, green: 241/225, blue: 53/225))
+                .clipShape(Capsule())
+                .foregroundColor(.black)
+            }
             
             Spacer()
             Divider()
             Spacer()
             
             Text("Received Messages")
-                .font(.headline)
+                .font(.title)
                 .foregroundStyle(Color(red: 196/225, green: 241/225, blue: 53/225))
             List {
                 ForEach(store.messages) { msg in
@@ -48,6 +77,10 @@ struct ContentView: View {
     }
     
     private func publish() async {
+        if message.isEmpty {
+            return
+        }
+        
         print("Going to publish \(message)")
         let result = await momentoClient.topicClient.publish(
             cacheName: momentoClient.cacheName,
@@ -60,6 +93,5 @@ struct ContentView: View {
         default:
             print("Error publishing: \(result)")
         }
-        message = ""
     }
 }
